6fef3f9f76127d298a3550c2eb3157978411ac01
hasibulislam999 <hasib143sl@gmail.com>
2022-06-03 16:50:58 +0600

    restify projects done

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,7 +8,7 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title><span className="purple">{props.title}</span></Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
